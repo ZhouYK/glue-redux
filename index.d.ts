e8e2fe253686ce990ab23020bebc74a2046f6270
yunkui.zhou <yunkui.zhou@bkjk.com>
2018-07-31 17:49:35 +0800

    添加ts声明文件

--- index.d.ts
+++ index.d.ts
@@ -2,9 +2,24 @@ interface Glue {
   [index:string]: any;
 }
 
-interface CreateGlueFnc<T> {
+interface Action {
+  type: string,
+  data: any
+}
+
+type ActionCreator = (data: any) => any;
+
+type Reducer = (state: any, action:Action ) => any;
+
+interface CreateGlue<T> {
   <T>(module: T, defaultValue?: T): T;
 }
-declare const createGlue: CreateGlueFnc<Glue>;
 
-declare const gluePair
+type Generator = () => Iterator;
+
+interface GluePair {
+  (actionCreator: ActionCreator, reducer: Reducer): Generator;
+}
+declare const createGlue: CreateGlue<Glue>;
+
+declare const gluePair:GluePair;
