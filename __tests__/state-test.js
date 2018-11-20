07c101c543b4336f84eb0e5250a1ff0d1ee0a639
yunkui.zhou <yunkui.zhou@bkjk.com>
2018-11-20 16:28:13 +0800

    补全测试

--- __tests__/state-test.js
+++ __tests__/state-test.js
@@ -5,6 +5,9 @@ import testModel from '../example/glue/modelTest';
 
 test('state test', () => {
   let initialState = store.getState();
+  let initialApp = referToState(appModel);
+  let initialProfile = referToState(appModel.profile);
+  let initialUsers = referToState(appModel.users);
   let tempState = referToState(wholeModel);
   expect(initialState).toBe(tempState);
 
@@ -22,6 +25,12 @@ test('state test', () => {
   tempState = referToState(wholeModel);
   expect(state).toBe(tempState);
 
+  let currentApp = referToState(appModel);
+  let currentProfilx = referToState(appModel.profile);
+  let currentUsers = referToState(appModel.users);
+  expect(Object.is(currentApp, initialApp)).toBe(false);
+  expect(currentProfilx).toBe(initialProfile);
+  expect(Object.is(currentUsers, initialUsers)).toBe(false);
   expect(Object.is(initialState, state)).toBe(false);
 
   expect(referToState(testModel)).toBe(undefined);
